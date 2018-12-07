9d039f08579d6e29e5b1ee6097fd5f12cde763be
RichyHBM <gitemail@richyhbm.co.uk>
2018-12-07 18:16:12 +0000

    Show permission activity when permission not granted

--- app/src/main/java/uk/co/richyhbm/monochromatic/Activities/MainActivity.kt
+++ app/src/main/java/uk/co/richyhbm/monochromatic/Activities/MainActivity.kt
@@ -1,12 +1,21 @@
 package uk.co.richyhbm.monochromatic.Activities
 
 import android.os.Bundle
+import android.widget.TextView
+import uk.co.richyhbm.monochromatic.Constants
 import uk.co.richyhbm.monochromatic.R
+import uk.co.richyhbm.monochromatic.Utilities.Permissions
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        when (Permissions.hasSecureSettingsPermission(this)) {
+            true  -> setContentView(R.layout.activity_main)
+            false -> {
+                setContentView(R.layout.activity_no_permissions)
+                findViewById<TextView>(R.id.adb_command_text)?.text = Constants.adbCommand
+            }
+        }
     }
 }
