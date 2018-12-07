6e8990a1c8342855d33be5f255770dbfaab4a9d3
RichyHBM <gitemail@richyhbm.co.uk>
2018-12-07 18:15:40 +0000

    Daltonised values

--- app/src/main/java/uk/co/richyhbm/monochromatic/Constants.kt
+++ app/src/main/java/uk/co/richyhbm/monochromatic/Constants.kt
@@ -6,4 +6,8 @@ object Constants {
 
     const val displayDaltonizerEnabled = "accessibility_display_daltonizer_enabled"
     const val displayDaltonizer = "accessibility_display_daltonizer"
+
+
+    const val daltonizerDisabled = -1
+    const val daltonizerSimulateMonochrome = 0
 }
