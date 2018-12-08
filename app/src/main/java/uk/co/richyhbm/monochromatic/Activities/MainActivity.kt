6cc9d6d5cba399fbb485195f1bd4b3659d490203
RichyHBM <gitemail@richyhbm.co.uk>
2018-12-08 15:41:19 +0000

    Use fragments

--- app/src/main/java/uk/co/richyhbm/monochromatic/Activities/MainActivity.kt
+++ app/src/main/java/uk/co/richyhbm/monochromatic/Activities/MainActivity.kt
@@ -1,36 +1,28 @@
 package uk.co.richyhbm.monochromatic.Activities
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
-import uk.co.richyhbm.monochromatic.Constants
+import uk.co.richyhbm.monochromatic.Fragments.MainFragment
+import uk.co.richyhbm.monochromatic.Fragments.NoPermissionsFragment
 import uk.co.richyhbm.monochromatic.R
 import uk.co.richyhbm.monochromatic.Utilities.Permissions
-
 
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        when (Permissions.hasSecureSettingsPermission(this)) {
-            true -> setContentView(R.layout.activity_main)
-            false -> noPermissionsActivity()
-        }
-    }
 
-    fun noPermissionsActivity() {
-        setContentView(R.layout.activity_no_permissions)
-        val adbCommandText = findViewById<TextView>(R.id.adb_command_text)
-        adbCommandText.text = Constants.getAdbCommand(this)
-        adbCommandText.setOnClickListener {
-            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("adb_command", Constants.getAdbCommand(this))
-            clipboard.primaryClip = clip
-            Toast.makeText(applicationContext, "Command copied", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            val fragment = when(Permissions.hasSecureSettingsPermission(this)) {
+                true -> MainFragment.newInstance()
+                false -> NoPermissionsFragment.newInstance()
+            }
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commitNow()
         }
     }
 }
---
+++ app/src/main/java/uk/co/richyhbm/monochromatic/Fragments/MainFragment.kt
@@ -0,0 +1,28 @@
+package uk.co.richyhbm.monochromatic.Fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uk.co.richyhbm.monochromatic.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
