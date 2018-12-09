57ab09612f526345f0d55d578932170edcdd2a28
RichyHBM <gitemail@richyhbm.co.uk>
2018-12-09 18:35:00 +0000

    Restart service on app update

---
+++ app/src/main/java/uk/co/richyhbm/monochromatic/Receivers/AppUpdatedReceiver.kt
@@ -0,0 +1,18 @@
+package uk.co.richyhbm.monochromatic.Receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import uk.co.richyhbm.monochromatic.Services.MonochromeService
+import uk.co.richyhbm.monochromatic.Utilities.Settings
+
+class AppUpdatedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val settings = Settings(context)
+
+        if (settings.isEnabled()) {
+            MonochromeService.startService(context)
+        }
+    }
+
+}
