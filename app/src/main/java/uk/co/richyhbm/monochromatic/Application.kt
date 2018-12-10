437d4cad43c65cccbc405fb95b843b064c52f093
RichyHBM <gitemail@richyhbm.co.uk>
2018-12-10 13:13:11 +0000

    Use day night style

--- app/src/main/java/uk/co/richyhbm/monochromatic/Application.kt
+++ app/src/main/java/uk/co/richyhbm/monochromatic/Application.kt
@@ -1,3 +1,17 @@
 package uk.co.richyhbm.monochromatic
 
-class Application : android.app.Application()
+import android.os.Build
+import android.support.v7.app.AppCompatDelegate
+
+class Application : android.app.Application() {
+    companion object {
+        init {
+            AppCompatDelegate.setDefaultNightMode(
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                else
+                    AppCompatDelegate.MODE_NIGHT_AUTO
+            )
+        }
+    }
+}
